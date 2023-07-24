33604f8d688e1ae58f147ee98b2ddea9f1623b15
johncuster <mark_john_custer@dlsu.edu.ph>
2023-07-24 16:13:12 +0800

    Update
    hbs and ccs file is working simultaneously.

--- src/index.js
+++ src/index.js
@@ -2,22 +2,43 @@ const express = require("express")
 const path = require("path")
 const app = express()
 
-const hbs = require("hbs")
+//const hbs = require("hbs")
 const collection = require("./mongodb")
-
-const templatePath = path.join(__dirname,'../templates')
+const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const templatePath = path.join(__dirname,'../templates')
+const publicPath = path.join(__dirname, '../public')
+const imagesPath = path.join(__dirname, '../images')
+
+console.log(publicPath);
+console.log(imagesPath);
+
 app.set("view engine", "hbs")
 app.set("views", templatePath)
-app.use(express.urlencoded({extended:false}))
+app.use(express.static(publicPath))
+app.use(express.static(imagesPath))
 
 app.get("/", (req, res) =>{
-    res.render("login")
+    res.render("home")
 })
 
 app.post("/login",async (req, res)=>{
-    
+    try{
+        const check = await collection.findOne({cname:req.body.cname})
+
+        if(check.password === req.body.password){
+            res.status(201).render("home", { naming: `${req.body.password}+${req.body.cname}` })
+        }
+        else{[
+            res.send("wrong password")
+        ]}
+    }
+    catch(e){
+        res.send("wrong details")
+    }
 })
 
 app.get("/signup", (req, res) =>{
@@ -31,9 +52,23 @@ app.post("/signup", async (req, res)=>{
         password:req.body.password
     }
 
-    await collection.insertMany([data])
+    const checking = await collection.findOne({cname: req.body.cname})
 
-    res.render("home")
+    try{
+        if(checking.cname === req.body.cname && checking.password === req.body.password){
+            res.send("user details already exists")
+        }
+        else{
+            await collection.insertMany([data]) 
+        }
+    }
+    catch{
+        res.send("wrong inputs")
+    }
+    
+    res.status(201).render("home", {
+        naming: req.body.cname
+    })
 })
 
 app.listen(3000, () =>{
